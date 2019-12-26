5a63c6287a0c4f4297a40156469a1462463b7458
VladSha-Usa <newshapovalov2002@gmail.com>
2019-12-27 00:59:20 +0200

    no message

---
+++ js/slider-main.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".slider-images").slick({
+    // autoplay:true,
+    // autoplaySpeed:2500,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows_left" src="img/arrow-left-slider.png" alt="" /></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows_right" src="img/arrow-right-slider.png" alt="" /></img>',
+      
+    })
+  });
